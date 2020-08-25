552046d96f834c8cca0a460826dfd5d3b0adacb3
Maximilian Gravemeyer <m.gravemeyer@icloud.com>
2020-08-25 23:06:01 +0200

    Added SignIn/SignUp Screen

--- Shared/LoginView.swift
+++ Shared/LoginView.swift
@@ -9,8 +9,26 @@ import SwiftUI
 import CoreData
 
 struct LoginView: View {
+    @State var username = String()
+    @State var password = String()
+    @State var response = String()
     var body: some View {
-        Text("Login")
+        HStack {
+            Image("Chimp_Logo").resizable().frame(width: 100, height: 100)
+            VStack {
+                TextField("Username", text: self.$username)
+                TextField("Password", text: self.$password)
+                Text(self.response)
+                HStack {
+                    Button("Sign In") {
+                        self.response = "SignedIn"
+                    }
+                    Button("Sign Up") {
+                        self.response = "SignedUp"
+                    }
+                }
+            }.frame(width: 200)
+        }.frame(minWidth: 400, idealWidth: 900, maxWidth: .infinity, minHeight: 200, idealHeight: 500, maxHeight: .infinity)
     }
 }
 
