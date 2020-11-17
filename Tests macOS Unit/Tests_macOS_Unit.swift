413fb2d9726b6f7f3080c8fdda905c7682b38ddc
Maximilian Gravemeyer <m.gravemeyer@icloud.com>
2020-11-17 16:56:30 +0100

    added unit tests file

---
+++ Tests macOS Unit/Tests_macOS_Unit.swift
@@ -0,0 +1,26 @@
+//
+//  Tests_macOS_Unit.swift
+//  Tests macOS Unit
+//
+//  Created by Maximilian Gravemeyer on 17.11.20.
+//
+
+import XCTest
+@testable import Chimp
+
+class Tests_macOS_Unit: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+}
