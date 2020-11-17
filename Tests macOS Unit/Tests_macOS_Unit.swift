edb6b00a074b5dcbdc396f1053abfd69b087f09f
Maximilian Gravemeyer <m.gravemeyer@icloud.com>
2020-11-17 16:57:01 +0100

    added in-memory coreData stack extension for tests

--- Tests macOS Unit/Tests_macOS_Unit.swift
+++ Tests macOS Unit/Tests_macOS_Unit.swift
@@ -24,3 +24,19 @@ class Tests_macOS_Unit: XCTestCase {
     }
 
 }
+
+//create in-memory CoreData stack for tests
+extension NSManagedObjectContext {
+    class func contextForTests() -> NSManagedObjectContext {
+        // Get the model
+        let model = NSManagedObjectModel.mergedModel(from: Bundle.allBundles)!
+        // Create and configure the coordinator
+        let coordinator = NSPersistentStoreCoordinator(managedObjectModel: model)
+        try! coordinator.addPersistentStore(ofType: NSInMemoryStoreType, configurationName: nil, at: nil, options: nil)
+        
+        // Setup the context
+        let context = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+        context.persistentStoreCoordinator = coordinator
+        return context
+    }
+}
