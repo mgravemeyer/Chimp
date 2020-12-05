8a57a5be116b6962249b15f050c47dfa16d86f78
m.gravemeyer@icloud.com <maximiliangravemeyer@Maximilians-MacBook-Air.local>
2020-12-05 18:45:54 +0100

    added test_getContactCategories

--- Tests macOS Unit/ContactsStateTests.swift
+++ Tests macOS Unit/ContactsStateTests.swift
@@ -11,7 +11,7 @@ class ContactsStateTests: XCTestCase {
     func test_createContact() throws {
         let contactsState = ContactsState()
         XCTAssertTrue(contactsState.contacts.count == 0)
-        let contact = Contact(firstname: "firstNameUI", lastname: "lastNameUI", email: "test@test.deUI", telephone: "016284392", birthday: "123456789", company: "companyUI")
+        let contact = Contact(firstname: "firstName", lastname: "lastName", email: "test@test.de", telephone: "016284392", birthday: "123456789", company: "company")
         contactsState.createContact(contact: contact)
         XCTAssertTrue(contactsState.contacts.count == 1)
     }
@@ -21,4 +21,14 @@ class ContactsStateTests: XCTestCase {
         contactsState.fetchContacts()
         XCTAssertEqual(contactsState.contacts, [])
     }
+    
+    func test_getContactCategories() throws {
+        let contactsState = ContactsState()
+        let contact = Contact(firstname: "firstNameUI", lastname: "lastNameUI", email: "test@test.deUI", telephone: "016284392", birthday: "123456789", company: "companyUI")
+        contactsState.createContact(contact: contact)
+        let categories = contactsState.getContactCategories()
+        XCTAssertNotNil(categories, "no contact categories found")
+        XCTAssertEqual(categories[0], "l")
+    }
 }
+ 
