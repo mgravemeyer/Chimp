6f02e26b434609e355eee5c141579a7b2d0a7145
m.gravemeyer@icloud.com <maximiliangravemeyer@Maximilians-MacBook-Air.local>
2020-12-05 20:43:41 +0100

    seperated CommandLineRowView from CommandLineView

---
+++ Shared/UI/Screens/CommandLineRowView.swift
@@ -0,0 +1,23 @@
+import SwiftUI
+
+struct CommandLineRowView: View {
+    @State var contact: Contact
+    @State var isHovered : Bool
+    var body: some View {
+        HStack {
+            Text(contact.firstname).font(.system(size: 18, weight: .medium))
+            Text(contact.lastname).font(.system(size: 18, weight: .medium))
+            Spacer()
+            if isHovered {
+                Text("Use with").font(.system(size: 14, weight: .medium))
+                Image(systemName: "return").font(.system(size: 14, weight: .medium))
+                Text("or").font(.system(size: 14, weight: .medium))
+                Image(systemName: "cursorarrow.rays").font(.system(size: 14, weight: .medium))
+            }
+            //This Line Is A Fix For Weird Padding Behavior in SwiftUI
+            Image(systemName: "cursorarrow.rays").font(.system(size: 14, weight: .medium)).opacity(0).padding(.trailing, -15)
+        }.padding(.top, 0).padding(.bottom, 0).frame(height: 50).font(.system(size: 18, weight: .medium)).font(.title).padding(.leading, 5).padding(.trailing, 5).background(isHovered ? Color(red: 240/255, green: 240/255, blue: 240/255) : Color.red.opacity(0)).cornerRadius(10).onHover { (isHovered) in
+            self.isHovered = isHovered
+        }
+    }
+}
