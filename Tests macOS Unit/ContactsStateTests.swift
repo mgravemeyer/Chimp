5fc1d37e742fec7108c5853ce09e249620d02a48
m.gravemeyer@icloud.com <maximiliangravemeyer@Maximilians-MacBook-Air.local>
2020-12-05 18:59:37 +0100

    added test_getContactsBySearch

--- Tests macOS Unit/ContactsStateTests.swift
+++ Tests macOS Unit/ContactsStateTests.swift
@@ -30,5 +30,16 @@ class ContactsStateTests: XCTestCase {
         XCTAssertNotNil(categories, "no contact categories found")
         XCTAssertEqual(categories[0], "l")
     }
+    
+    func test_getContactsBySearch() throws {
+        let contactsState = ContactsState()
+        let searchResultEmpty = contactsState.getContactsBySearch(search: "l")
+        XCTAssertEqual(searchResultEmpty, [Contact]())
+        let contact = Contact(firstname: "firstNameUI", lastname: "lastNameUI", email: "test@test.deUI", telephone: "016284392", birthday: "123456789", company: "companyUI")
+        contactsState.createContact(contact: contact)
+        let searchResult = contactsState.getContactsBySearch(search: "f")
+        XCTAssertFalse(searchResult.count == 0)
+        XCTAssertTrue(contactsState.contacts.contains(searchResult[0]))
+    }
 }
  
