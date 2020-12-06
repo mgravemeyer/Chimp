139d40bfe775f291c2df8ccacfc0ac9a54a42872
m.gravemeyer@icloud.com <maximiliangravemeyer@Maximilians-MacBook-Air.local>
2020-12-06 10:50:15 +0100

    separated PreviewCoreDataWrapper struct from ChimpApp+Wrapper.

--- Shared/UI/Screens/ChimpApp+Wrapper.swift
+++ Shared/UI/Screens/ChimpApp+Wrapper.swift
@@ -57,14 +57,3 @@ struct AppWrapper_Previews : PreviewProvider {
     }
 }
 #endif
-
-struct PreviewCoreDataWrapper<Content: View>: View {
-    let content: Content
-    init(@ViewBuilder content: @escaping () -> Content) {
-            self.content = content()
-        CoreDataManager.shared.changeToDevelopmentMode()
-        }
-  var body: some View {
-    content
-  }
-}
