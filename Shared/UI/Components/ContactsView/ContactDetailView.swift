5902ba663f90c9cbc546e6a7a133666c56e8ab84
m.gravemeyer@icloud.com <m.gravemeyer@icloud.com>
2020-12-20 23:11:28 +0100

    added horizontal radio buttons to contact details

--- Shared/UI/Components/ContactsView/ContactDetailView.swift
+++ Shared/UI/Components/ContactsView/ContactDetailView.swift
@@ -21,6 +21,11 @@ struct ContactDetailView: View {
                     .buttonStyle(PlainButtonStyle())
                     .padding(.bottom, 10)
                     ContactsDetailTagRow(selectedContact: contact).padding(.bottom, 12)
+                HStack {
+                    Spacer()
+                    ContactsDetailRadioSection()
+                    Spacer()
+                }
                 Spacer()
             }.zIndex(1)
             Rectangle().foregroundColor(colorScheme == .dark ? Color.chimpDarkBackground : Color.chimpLightBackground).zIndex(0)
@@ -57,6 +62,78 @@ struct ContactsDetailContactRow: View {
                 }
             })
         }
+    }
+}
+
+struct RoundedCorners: Shape {
+    var tl: CGFloat = 0.0
+    var tr: CGFloat = 0.0
+    var bl: CGFloat = 0.0
+    var br: CGFloat = 0.0
+
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+
+        let w = rect.size.width
+        let h = rect.size.height
+
+        // Make sure we do not exceed the size of the rectangle
+        let tr = min(min(self.tr, h/2), w/2)
+        let tl = min(min(self.tl, h/2), w/2)
+        let bl = min(min(self.bl, h/2), w/2)
+        let br = min(min(self.br, h/2), w/2)
+
+        path.move(to: CGPoint(x: w / 2.0, y: 0))
+        path.addLine(to: CGPoint(x: w - tr, y: 0))
+        path.addArc(center: CGPoint(x: w - tr, y: tr), radius: tr,
+                    startAngle: Angle(degrees: -90), endAngle: Angle(degrees: 0), clockwise: false)
+
+        path.addLine(to: CGPoint(x: w, y: h - br))
+        path.addArc(center: CGPoint(x: w - br, y: h - br), radius: br,
+                    startAngle: Angle(degrees: 0), endAngle: Angle(degrees: 90), clockwise: false)
+
+        path.addLine(to: CGPoint(x: bl, y: h))
+        path.addArc(center: CGPoint(x: bl, y: h - bl), radius: bl,
+                    startAngle: Angle(degrees: 90), endAngle: Angle(degrees: 180), clockwise: false)
+
+        path.addLine(to: CGPoint(x: 0, y: tl))
+        path.addArc(center: CGPoint(x: tl, y: tl), radius: tl,
+                    startAngle: Angle(degrees: 180), endAngle: Angle(degrees: 270), clockwise: false)
+
+        return path
+    }
+}
+
+struct ContactsDetailRadioSection: View {
+    @State var selection = "Notes"
+    var corner = CGFloat(10)
+    var body: some View {
+        HStack {
+            Button(action: {
+                
+            }, label: {
+                ZStack {
+                    Text("Notes").zIndex(1).foregroundColor(Color.white)
+                    RoundedCorners(tl: 10, tr: 0, bl: 10, br: 0).foregroundColor(Color.gray)
+                }
+            }).buttonStyle(PlainButtonStyle()).frame(height: 25).offset(x: 10)
+            Button(action: {
+                
+            }, label: {
+                ZStack {
+                    Text("E-Mails").zIndex(1).foregroundColor(Color.white)
+                    RoundedCorners(tl: 0, tr: 0, bl: 0, br: 0).foregroundColor(Color.gray)
+                }
+            }).buttonStyle(PlainButtonStyle()).frame(height: 25)
+            Button(action: {
+                
+            }, label: {
+                ZStack {
+                    Text("Calls").zIndex(1).foregroundColor(Color.white)
+                    RoundedCorners(tl: 0, tr: 10, bl: 0, br: 10).foregroundColor(Color.gray)
+                }
+            }).buttonStyle(PlainButtonStyle()).frame(height: 25).offset(x: -10)
+        }.frame(width: 200)
     }
 }
 
