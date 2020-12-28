101ec45604803910f6774ba169847f68f6f31963
m.gravemeyer@icloud.com <m.gravemeyer@icloud.com>
2020-12-28 18:09:48 +0100

    added addProject func

--- Shared/State/ProjectsState.swift
+++ Shared/State/ProjectsState.swift
@@ -4,4 +4,8 @@ import SwiftUI
 
 class ProjectsState: ObservableObject {
     var projects = [Project]()
+    
+    func addProject(name: String, progress: Int) {
+        projects.append(Project(name: name, progress: progress))
+    }
 }
