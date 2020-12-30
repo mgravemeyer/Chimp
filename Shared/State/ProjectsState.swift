f57b6482e4cf4c9c18d80014f869ce6da7942d0e
m.gravemeyer@icloud.com <m.gravemeyer@icloud.com>
2020-12-30 13:00:58 +0100

    resolved typo in ProjectsState

--- Shared/State/ProjectsState.swift
+++ Shared/State/ProjectsState.swift
@@ -5,7 +5,7 @@ import SwiftUI
 class ProjectsState: ObservableObject {
     
     @Published var projects = [Project]()
-`   @Published var addMenuePressed = false
+    @Published var addMenuePressed = false
     
     func addProject(name: String, progress: Int) {
         projects.append(Project(name: name, progress: progress))
